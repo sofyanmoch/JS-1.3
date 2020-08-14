b058925042e9f15f308a46f7fa8a719a83b5f112
sofyan-dev <mchsfyan17@gmail.com>
2020-08-14 09:14:12 +0700

    2

--- Task/fetchAPI.js
+++ Task/fetchAPI.js
@@ -3,9 +3,4 @@ const fetch = require('node-fetch')
 fetch('https://jsonplaceholder.typicode.com/users/')
 
 .then(res => res.json())
-.then(json => json.map(users => (
-    {
-        name: users.name
-    }
-)))
-.then(result => console.log(result))
+.then(json => json.map(users => console.log(users.name)))
